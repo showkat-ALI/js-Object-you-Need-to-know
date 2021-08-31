864f5919657f38c54121155c7bd70e3b437265fa
showkat-ALI <cpianshowkat@gmail.com>
2021-08-31 20:18:58 +0600

    object method

--- method.js
+++ method.js
@@ -1,9 +1,24 @@
 /* ----------- way to crate method in object ----- */
-const rich = {
+/* const rich = {
     name:"Elon Musk",
     age: undefined,
     business: function(company,revenue){
         return company +' '+" revenue is" + ' ' + revenue;
+    }
+
+
+    
+
+
+} */
+// console.log(rich.business("starlink",1));
+/*  -- way to access property of an onject in method -- */
+
+const rich = {
+    name:"Elon Musk",
+    age: undefined,
+    business: function(company,revenue){
+        return this.name +' '+ company +' '+" revenue is" + ' ' + revenue;
     }
 
 
